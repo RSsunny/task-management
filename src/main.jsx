bd5413decc60e59be99f8b5136b57a896649f92b
RSsunny <sunnysharif154@gmail.com>
2023-12-21 13:27:24 +0600

    set up all drectory and create router

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Router from "./router/Router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={Router}></RouterProvider>
+  </React.StrictMode>
 );
